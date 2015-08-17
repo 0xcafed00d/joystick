a0a61af5f8557ab48cda8920c42f1d8d1dada7dc
Lee Witek <neomech@gmail.com>
2015-08-17 01:37:54 +0100

    documenting

--- joystick_common.go
+++ joystick_common.go
@@ -1,8 +1,11 @@
+// Package joystick implements a Polled API to read the state of an attached joystick.
+// currently Windows & Linux are supported.
 package joystick
 
+// State holds the current state of the joystick
 type State struct {
-	AxisData []int
-	Buttons  uint32
+	AxisData []int  // Value of each axis as an integer in the range -32767 to 32768
+	Buttons  uint32 // The state of each button as a bit in a 32 bit integer. 1 = pressed, 0 = not pressed
 }
 
 type Joystick interface {
